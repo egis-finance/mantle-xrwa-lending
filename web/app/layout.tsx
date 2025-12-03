a8b20e9e690f453dc41b626ec61b5a482c7d2e48
Mario Karagiorgas <salem8@gmail.com>
2025-12-03 03:35:19 +0200

    fix(web): suppress hydration warning in layout

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -26,7 +26,7 @@ export default async function RootLayout({
 
   return (
     <html lang="en" className={`${sans.variable} ${serif.variable}`}>
-      <body>
+      <body suppressHydrationWarning>
         <Providers cookies={cookies}>{children}</Providers>
       </body>
     </html>
